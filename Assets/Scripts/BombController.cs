9ea9bb62b03d31b98d15d14c060ea3f878a2596d
Raíssa Juliana <rjgalbino@uniara.edu.br>
2022-12-29 13:03:59 -0300

    Explosions

--- Assets/Scripts/BombController.cs
+++ Assets/Scripts/BombController.cs
@@ -3,11 +3,18 @@ using UnityEngine;
 
 public class BombController : MonoBehaviour
 {
+    [Header("Bomb")]
     public KeyCode inputKey = KeyCode.Space;
     public GameObject bombPrefab;
     public float bombFuseTime = 3f;
     public int bombAmount = 1;
     private int bombsRemaining;
+
+    [Header("Explosion")]
+    public Explosion explosionPrefab;
+    public LayerMask explosionLayerMask;
+    public float explosionDuration = 1f;
+    public int explosionRadius = 1;
 
     private void OnEnable()
     {
@@ -33,9 +40,43 @@ public class BombController : MonoBehaviour
 
         yield return new WaitForSeconds(bombFuseTime);
 
-        Destroy(bomb); //temporary
+        position = bomb.transform.position;
+        position.x = Mathf.Round(position.x);
+        position.y = Mathf.Round(position.y);
+
+        Explosion explosion = Instantiate(explosionPrefab, position, Quaternion.identity);
+        explosion.SetActiveRenderer(explosion.start);
+        explosion.DestroyAfter(explosionDuration);
+
+        Explode(position, Vector2.up, explosionRadius);
+        Explode(position, Vector2.down, explosionRadius);
+        Explode(position, Vector2.left, explosionRadius);
+        Explode(position, Vector2.right, explosionRadius);
+
+        Destroy(bomb); 
         bombsRemaining++;
-        //...
+    }
+
+    private void Explode(Vector2 position, Vector2 direction, int lenght)
+    {
+        if (lenght <= 0)
+        {
+            return;
+        }
+
+        position += direction;
+
+        if (Physics2D.OverlapBox(position, Vector2.one /2f, 0f, explosionLayerMask))
+        {
+            return;
+        }
+
+        Explosion explosion = Instantiate(explosionPrefab, position, Quaternion.identity);
+        explosion.SetActiveRenderer(lenght > 1 ? explosion.middle : explosion.end);
+        explosion.SetDirection(direction);
+        explosion.DestroyAfter(explosionDuration);
+
+        Explode(position, direction, lenght -1);
     }
 
     private void OnTriggerExit2D(Collider2D other)
@@ -45,4 +86,6 @@ public class BombController : MonoBehaviour
             other.isTrigger = false;
         }
     }
+
+
 }
